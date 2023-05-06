ccc83cb83d24b3ea98109c55e7508d6169000be3
swapv24 <swapnilverlekar24@gmail.com>
2023-05-06 10:42:48 -0400

    Latency Jitter folder added

---
+++ latency_jitter_testing/latency_jitter_client.py
@@ -0,0 +1,41 @@
+import socket
+import struct
+import pyaudio
+import time
+
+def start_client(ip, port):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    multicast_group = (ip, port)
+    client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    client_socket.bind(('', port))
+    group = socket.inet_aton(ip)
+    mreq = struct.pack('4sL', group, socket.INADDR_ANY)
+    client_socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+    p = pyaudio.PyAudio()
+    stream = p.open(format=p.get_format_from_width(2), channels=1, rate=44100, output=True)
+
+    print("Client started and joined multicast group")
+
+    prev_latency = None
+    while True:
+        try:
+            music = client_socket.recv(65507)
+            timestamp, music = struct.unpack('<d', music[:8])[0], music[8:]
+            latency = time.time() - timestamp
+
+            if prev_latency is not None:
+                jitter = abs(latency - prev_latency)
+                print(f'Jitter: {jitter:.6f} seconds')
+
+            prev_latency = latency
+
+            print(f'Latency: {latency:.6f} seconds')
+            stream.write(music)
+            print("Received and playing music data")
+        except Exception as e:
+            print(f"Error occurred: {e}")
+
+if __name__ == '__main__':
+    start_client('224.0.0.2', 9999)
---
+++ latency_jitter_testing/latency_jitter_server.py
@@ -0,0 +1,62 @@
+import socket
+import random
+import threading
+import math
+import struct
+import time
+
+def generate_music():
+    frequency = random.randint(100, 1000) # random frequency between 100 and 1000 Hz
+    sample_rate = 44100
+    duration = 0.5
+    num_samples = int(sample_rate * duration)
+
+    data = []
+    for i in range(num_samples):
+        sample = math.sin(2 * math.pi * frequency * i / sample_rate)
+        data.append(struct.pack('<h', int(sample * 32767)))
+
+    return b''.join(data)
+
+def send_music(multicast_socket, multicast_group, playback_state):
+    try:
+        while True:
+            if playback_state['paused']:
+                continue
+
+            music = generate_music()
+            timestamp = time.time()
+            data = struct.pack('<d', timestamp) + music
+            multicast_socket.sendto(data, multicast_group)
+
+    except Exception as e:
+        print(f"Error occurred: {e}")
+    finally:
+        multicast_socket.close()
+
+def handle_commands(playback_state):
+    while True:
+        command = input('Enter command (PAUSE/PLAY): ')
+        if command == 'PAUSE':
+            playback_state['paused'] = True
+        elif command == 'PLAY':
+            playback_state['paused'] = False
+
+def start_multicast_server(ip, port):
+    multicast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+    multicast_group = (ip, port)
+    multicast_socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
+
+    playback_state = {'paused': False}
+
+    command_thread = threading.Thread(target=handle_commands, args=(playback_state,))
+    command_thread.start()
+
+    print("Server started")
+
+    send_music(multicast_socket, multicast_group, playback_state)
+
+if __name__ == '__main__':
+    start_multicast_server('224.0.0.2', 9999)
